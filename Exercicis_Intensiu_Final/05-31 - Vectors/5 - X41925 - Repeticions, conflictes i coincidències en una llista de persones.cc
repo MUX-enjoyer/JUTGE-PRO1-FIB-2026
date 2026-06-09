128f53f9ff140fc3688a6d5b3c04c24edf892e70
RogeR Bitlloch <roger.bitlloch@estudiantat.upc.edu>
2026-06-09 23:14:13 +0200

    Nova versió X41925

--- Exercicis_Intensiu_Final/05-31 - Vectors/5 - X41925 - Repeticions, conflictes i coincidències en una llista de persones.cc
+++ Exercicis_Intensiu_Final/05-31 - Vectors/5 - X41925 - Repeticions, conflictes i coincidències en una llista de persones.cc
@@ -8,25 +8,21 @@ struct Persona {
     string nom;
 };
 
-bool cmp(const Persona &a, const Persona &b) {
+bool cmp_id(const Persona &a, const Persona &b) {
     if (a.id < b.id) return true;
-
     else if (a.id == b.id) {
         if (a.nom < b.nom) return true;
         else return false;
     }
-
     else return false;
 }
 
-bool cmp2(const Persona &a, const Persona &b) {
+bool cmp_nom(const Persona &a, const Persona &b) {
     if (a.nom < b.nom) return true;
-
     else if (a.nom == b.nom) {
         if (a.id < b.id) return true;
         else return false;
     }
-
     else return false;
 }
 
@@ -34,46 +30,49 @@ int main() {
     int n;
     while (cin >> n) {
         vector<Persona> persones(n);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
             cin >> persones[i].id >> persones[i].nom;
-        }
 
-        int repeticions = 0;    // id i nom igual
-        int coincidencies = 0;  // id igual però nom diferent
-        sort(persones.begin(), persones.end(), cmp);
+        // --- Ordenem per (id, nom): repeticions i conflictes ---
+        sort(persones.begin(), persones.end(), cmp_id);
 
-        int id_anterior = persones[0].id;
-        string nom_anterior = persones[0].nom;
+        int repeticions = 0;
+        int conflictes  = 0;
+        int cnt_id_nom  = 1; // mida del grup actual (id, nom)
+        int cnt_id = 1; // mida del grup actual id
         for (int i = 1; i < n; ++i) {
-            if (persones[i].id == id_anterior) {
-                if (persones[i].nom == nom_anterior) {
-                    ++repeticions;
-                }
-                else {
-                    ++coincidencies;
-                }
+            if (persones[i].id != persones[i-1].id) {
+                cnt_id_nom = 1;
+                cnt_id = 1;
             }
             else {
-                id_anterior = persones[i].id;
-                nom_anterior = persones[i].nom;
+                if (persones[i].nom == persones[i-1].nom) ++cnt_id_nom;
+                else cnt_id_nom = 1;
+                ++cnt_id;
+                repeticions += cnt_id_nom - 1;      // parelles amb mateix (id, nom)
+                conflictes += cnt_id - cnt_id_nom; // parelles amb mateix id, nom diferent
             }
         }
 
-        int conflictes = 0;     // id diferent però nom igual
-        sort(persones.begin(), persones.end(), cmp2);
-        int id_anterior = persones[0].id;
-        string nom_anterior = persones[0].nom;
+        // --- Ordenem per (nom, id): coincidències ---
+        sort(persones.begin(), persones.end(), cmp_nom);
+
+        int coincidencies = 0;
+        int cnt_nom_id = 1; // mida del grup actual (nom, id)
+        int cnt_nom = 1; // mida del grup actual nom
         for (int i = 1; i < n; ++i) {
-            if (persones[i].nom == nom_anterior) {
-                if (persones[i].id != id_anterior) {
-                    ++conflictes;
-                }
+            if (persones[i].nom != persones[i-1].nom) {
+                cnt_nom_id = 1;
+                cnt_nom = 1;
             }
             else {
-                id_anterior = persones[i].id;
-                nom_anterior = persones[i].nom;
+                if (persones[i].id == persones[i-1].id) ++cnt_nom_id;
+                else cnt_nom_id = 1;
+                ++cnt_nom;
+                coincidencies += cnt_nom - cnt_nom_id; // parelles amb mateix nom, id diferent
             }
         }
 
+        cout << repeticions << " " << conflictes << " " << coincidencies << "\n";
     }
 }
